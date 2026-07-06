3563adb5c07981d10e8c823418fb1fa8ee0770c8
William <68315131+SCUMBAG0LEE@users.noreply.github.com>
2026-07-06 23:16:14 +0700

    reverted the hacky workaround in the Dockerfile
    - Added os.O_TRUNC to ensure the file is completely overwritten correctly every time.
    - Added a fallback directly in the Go script so that if the Git command fails and returns an empty string (like it does on Heroku), it will use "heroku_build_no_git_hash" instead of an empty string. This prevents the runtime panic you initially saw.

--- server/generator/constants.go
+++ server/generator/constants.go
@@ -14,8 +14,13 @@ func main() {
 	cmd, b := exec.Command("git", "rev-parse", "HEAD"), new(strings.Builder)
 	cmd.Stdout = b
 	cmd.Run()
+	
+	buildRef := strings.TrimSpace(b.String())
+	if buildRef == "" {
+		buildRef = "heroku_build_no_git_hash"
+	}
 
-	f, err := os.OpenFile("../common/constants_generated.go", os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	f, err := os.OpenFile("../common/constants_generated.go", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, os.ModePerm)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
@@ -28,6 +33,6 @@ func init() {
     BUILD_REF = "%s"
     BUILD_DATE = "%s"
 }
-	`, strings.TrimSpace(b.String()), time.Now().Format("20060102"))))
+	`, buildRef, time.Now().Format("20060102"))))
 	f.Close()
 }
